53103f92c3d6a9fa106576b0df9c675bfcbf4041
josepaniagua <jpanjr@gmail.com>
2019-09-06 21:17:07 -0700

    deleted files

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,9 +5,12 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 1;
+    // const id = 1;
+    const { match } = props;
+    const id = match.params.movieId;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+    const movie = movie.find(movie => `${movie.id}` === id);
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
@@ -21,10 +24,10 @@ const Movie = (props) => {
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  };
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -49,9 +52,9 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
-}
+};
 
 export default Movie;
